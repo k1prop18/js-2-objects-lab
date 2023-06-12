4a95c161197f86a56784e9c28da45dee50e27667
Caleb Kiprop Rono <ckiprop14@gmail.com>
2023-06-12 22:48:22 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: 'John Doe',
+  streetAddress: '123 Main Street',
+};
+
+  
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const clone = Object.assign({}, employee);
+    clone[key] = value;
+    return clone;
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const clone = Object.assign({}, employee);
+    delete clone[key];
+    return clone;
+  }
+  
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
